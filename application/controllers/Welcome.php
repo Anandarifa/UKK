055b660b61e8805b2e3d9b0d1f8e6e88431305cb
Anandarifa <nandarifadila@gmail.com>
2018-01-26 16:59:29 +0700

    Sprint 1 1,2,3

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('index');
+	}
+	function add_user(){
+		$data = array(
+			'username' => $this->input->post('username'),
+			'password' =>md5($this->input->post('password')),
+			'fullname' => $this->input->post('fullname'),
+			'level' => $this->input->post('level'),
+			);
+		$result = $this->model->Simpan('user', $data);
+		if($result == 1){
+			$this->session->set_flashdata("sukses", "<div class='alert alert-success'><strong>Simpan data BERHASIL dilakukan</strong></div>");
+			header('location:'.base_url());
+		}else{
+			$this->session->set_flashdata("alert", "<div class='alert alert-danger'><strong>Simpan data GAGAL di lakukan</strong></div>");
+			header('location:'.base_url());
+		}
+	}
+
+}
